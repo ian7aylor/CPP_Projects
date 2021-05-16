58a067e38d6aa0dd5f85dcba2a40b05943b8776c
ian7aylor <ian7aylor@gmail.com>
2021-05-16 00:07:19 -0700

    Setting Up VSCode

---
+++ HelloWorld.cpp
@@ -0,0 +1,18 @@
+
+
+#include <stdio.h>
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main(){
+string item;
+cout << "Hello World" << endl;
+
+
+//User input another string of their choice
+getline(cin, item);
+
+cout << item;
+
+}
